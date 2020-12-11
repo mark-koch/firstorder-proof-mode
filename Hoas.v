37ec2b54b73c8e4713ff62e6f0e19f89b3c861c6
Mark Koch <s8mkkoch@stud.uni-saarland.de>
2020-12-11 21:46:56 +0100

    Fix hoas coercion

--- Hoas.v
+++ Hoas.v
@@ -16,16 +16,12 @@ Inductive bform `{funcs_signature, preds_signature, operators} :=
   | bQuant : quantop -> (bterm -> bform)  -> bform
   | bFree : (bterm -> bform)  -> bform.
 
-(* Coercion bterm_cor := bEmbedT. *)
-
 Fixpoint conv_term `{funcs_signature} i (b : bterm) : term :=
   match b with
   | bVar m => var (i - m)
   | bEmbedT t => t
   | bFunc f v => func f (Vector.map (conv_term i) v)
   end.
-
-Coercion bEmbedT : term >-> bterm.
 
 Fixpoint conv `{funcs_signature, preds_signature, operators} i (phi : bform) : form :=
   match phi with
@@ -42,26 +38,28 @@ Delimit Scope hoas_scope with hoas.
 Notation "'Free' x .. y , p" := (bFree Ex (fun x => .. (bFree Ex (fun y => p)) ..))
 (at level 50, x binder, left associativity,
   format "'[' 'Free'  '/  ' x  ..  y ,  '/  ' p ']'") : hoas_scope.
-Notation "'∀' x .. y , p" := (bQuant All (fun x => .. (bQuant All (fun y => p)) ..))
+Notation "'∀'' x .. y , p" := (bQuant All (fun x => .. (bQuant All (fun y => p)) ..))
+(at level 50, x binder,  left associativity,
+  format "'[' '∀''  '/  ' x  ..  y ,  '/  ' p ']'") : hoas_scope.
+Notation "'∃'' x .. y , p" := (bQuant Ex (fun x => .. (bQuant Ex (fun y => p)) ..))
 (at level 50, x binder, left associativity,
-  format "'[' '∀'  '/  ' x  ..  y ,  '/  ' p ']'") : hoas_scope.
-Notation "'∃' x .. y , p" := (bQuant Ex (fun x => .. (bQuant Ex (fun y => p)) ..))
-(at level 50, x binder, left associativity,
-  format "'[' '∃'  '/  ' x  ..  y ,  '/  ' p ']'") : hoas_scope.
+  format "'[' '∃''  '/  ' x  ..  y ,  '/  ' p ']'") : hoas_scope.
 Notation "⊥" := (bFal) : hoas_scope.
 Notation "A ∧ B" := (bBin Conj A B) (at level 41) : hoas_scope.
 Notation "A ∨ B" := (bBin Disj A B) (at level 42) : hoas_scope.
 Notation "A '-->' B" := (bBin Impl A B) (at level 43, right associativity) : hoas_scope.
 
-Notation "'σ' x" := (@bFunc PA_funcs_signature Succ (Vector.cons x (Vector.nil bterm))) (at level 37) : hoas_scope.
-Notation "x '⊕' y" := (@bFunc PA_funcs_signature Plus (Vector.cons x (Vector.cons y (Vector.nil bterm))) ) (at level 39) : hoas_scope.
-Notation "x '⊗' y" := (@bFunc PA_funcs_signature Mult (Vector.cons x (Vector.cons y (Vector.nil bterm))) ) (at level 38) : hoas_scope. 
-Notation "x '==' y" := (@bAtom PA_funcs_signature PA_preds_signature _ Eq (Vector.cons bterm x 1 (Vector.cons bterm y 0 (Vector.nil bterm))) ) (at level 40) : hoas_scope.
-
 Definition convert `{funcs_signature, preds_signature, operators} f := (@conv _ _ _ 0 f).
 Arguments convert {_ _ _} f%hoas.
 
 Notation "<< f" := (ltac:(let y := eval cbn -[subst_form] in (convert f) in exact y)) (at level 200, only parsing).
+
+
+Coercion bEmbedT : term >-> bterm.
+Definition vec_term := Vector.t term.
+Definition vec_bterm := Vector.t bterm.
+Definition vec_bEmbedT' : forall n, vec_term n -> vec_bterm n := @Vector.map term bterm bEmbedT.
+Coercion vec_bEmbedT := vec_bEmbedT'.
 
 
 (* 
@@ -71,9 +69,12 @@ Section Test.
 
   Variable p : peirce.
 
-  (* TODO: Why ist the `%hoas` needed? *)
-  Goal nil ⊢ << (∀ a, a == a)%hoas.
+  Goal forall phi, nil ⊢ ∀ phi.
   Abort.
 
-End Test. 
+  (* TODO: Why ist the `%hoas` needed? *)
+  Goal nil ⊢ << (∀' a, a == a)%hoas.
+  Abort.
+
+End Test.
 *)
